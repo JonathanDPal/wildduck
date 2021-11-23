8f786f9239407f946339a4cf3a7fb1a2085ca33b
JonathanDPal <jpal@nd.edu>
2021-11-23 15:10:57 -0600

    stuff that produced first HR diagram (not quite there yet)

--- flux_matching.py
+++ flux_matching.py
@@ -1,14 +1,20 @@
 import numpy as np
 import pandas as pd
+import astroalign as aa
+from astropy.io import fits
 
 # USER INPUTS #
+# Original images that flux measurements were made on
+r_imagefile = 'wild_duck/WildDuck-R-stack.fit'
+b_imagefile = 'wild_duck/WildDuck-B-stack.fit'
+v_imagefile = 'wild_duck/WildDuck-V-stack.fit'
 # CSV files that were output of 'flux_measurement.py'
-r_fluxtable = ''
-b_fluxtable = ''
-v_fluxtable = ''
-FWHM = 3
-num_sat_stars = 3
-output_file = ''
+r_fluxtable = 'wild_duck/wild_duck-Rfluxes.csv'
+b_fluxtable = 'wild_duck/wild_duck-Bfluxes.csv'
+v_fluxtable = 'wild_duck/wild_duck-Vfluxes.csv'
+FWHM = 8.5
+num_sat_stars = 5  # CANNOT BE ZERO
+output_file = 'wild_duck/wild_duck-fluxes.csv'
 # END OF USER INPUTS #
 
 
@@ -16,33 +22,49 @@ def distance2(loc1, loc2):
     """
     Distance squared
     """
-    x0, y0, _ = loc1
-    x1, y1, _ = loc2
+    x0, y0 = loc1
+    x1, y1 = loc2
     return (x1 - x0) ** 2 + (y1 - y0) ** 2
 
 
-r_fluxes = pd.read_csv(r_fluxtable).sort_values('Flux', ascending=True)[:num_sat_stars]
-b_fluxes = pd.read_csv(b_fluxtable).sort_values('Flux', ascending=True)[:num_sat_stars]
-v_fluxes = pd.read_csv(v_fluxtable).sort_values('Flux', ascending=True)[:num_sat_stars]
+v_fluxes = pd.read_csv(v_fluxtable).sort_values('Flux', ascending=True)[:-1 * num_sat_stars]
+r_fluxes = pd.read_csv(r_fluxtable).sort_values('Flux', ascending=True)[:-1 * num_sat_stars]
+b_fluxes = pd.read_csv(b_fluxtable).sort_values('Flux', ascending=True)[:-1 * num_sat_stars]
+
+with fits.open(r_imagefile) as f:
+    r_img = f[0].data
+with fits.open(b_imagefile) as f:
+    b_img = f[0].data
+with fits.open(v_imagefile) as f:
+    v_img = f[0].data
+
+vr_transform, (r_locs, v_locs1) = aa.find_transform(r_img, v_img)
+vb_transform, (b_locs, v_locs2) = aa.find_transform(b_img, v_img)
 
 compiled_data = {'X': list(), 'Y': list(), 'R Flux': list(), 'B Flux': list(), 'V Flux': list()}
-for _, row in r_fluxes.iterrows():
+for _, row in v_fluxes.iterrows():
     loc = (row['X'], row['Y'])
+    r_locT = aa.matrix_transform(loc, vr_transform.params)
+    r_locT = (r_locT[0, 0], r_locT[0, 1])
+    seps2 = [distance2(loc, vloc2) for vloc2 in v_locs2]
+    b_locT = aa.matrix_transform(loc, vb_transform.params)
+    b_locT = (b_locT[0, 0], b_locT[0, 1])
     b_flux = []
-    v_flux = []
+    r_flux = []
     for _, b_row in b_fluxes.iterrows():
         b_loc = (b_row['X'], b_row['Y'])
-        if distance2(b_loc, loc) < FWHM:
+        if distance2(b_loc, b_locT) < FWHM:
             b_flux.append(b_row['Flux'])
-    for _, v_row in v_fluxes.iterrows():
-        v_loc = (v_row['X'], v_row['Y'])
-        if distance2(v_loc, loc) < FWHM:
-            v_flux.append(v_row['Flux'])
-    if len(b_flux) == 1 and len(v_flux) == 1:
+    for _, r_row in r_fluxes.iterrows():
+        r_loc = (r_row['X'], r_row['Y'])
+        if distance2(r_loc, r_locT) < FWHM:
+            r_flux.append(r_row['Flux'])
+    if len(b_flux) == 1 and len(r_flux) == 1:
         compiled_data['X'].append(loc[0])
         compiled_data['Y'].append(loc[1])
-        compiled_data['R Flux'].append(row['Flux'])
+        compiled_data['R Flux'].append(r_flux[0])
         compiled_data['B Flux'].append(b_flux[0])
-        compiled_data['V Flux'].append(v_flux[0])
+        compiled_data['V Flux'].append(row['Flux'])
 
-pd.DataFrame(compiled_data).to_csv('matchedfluxes.csv', index=False)
+pd.DataFrame(compiled_data).to_csv(output_file, index=False)
+w
--- hr_generator.py
+++ hr_generator.py
@@ -3,17 +3,18 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # USER INPUTS #
-input_file = ''
+input_file = 'wild_duck/wild_duck-fluxes.csv'
 filter1 = 'B'
 filter2 = 'V'
-output_image = ''
+output_image = 'wild_duck/wild_duck-HRdiagram.png'
 # END OF USER INPUTS #
 
-fluxes = pd.read_csv(input_file)
+fluxes = pd.read_csv(input_file).sort_values('V Flux', ascending=False)
+fluxes = fluxes[:int(np.ceil(len(fluxes) / 5))]
 ref_flux = fluxes['V Flux'].median()
 fluxes['Magnitude'] = [-2.5 * np.log10(flux / ref_flux) for flux in fluxes['V Flux']]
-fluxes['Color'] = [-2.5 * np.log10(row['B Flux'] / row['V Flux']) for _, row in fluxes.iterrows()]
-fluxes.plot(x='Color', y='Magnitude', kind='scatter', use_index=False)
-plt.xlabel('B - V (mag)')
+fluxes['Color'] = [-2.5 * np.log10(row[f'{filter1} Flux'] / row[f'{filter2} Flux']) for _, row in fluxes.iterrows()]
+fluxes.plot(x='Color', y='Magnitude', kind='scatter', use_index=False, ylim=(-1, 0.5))
+plt.xlabel(f'{filter1} - {filter2} (mag)')
 plt.ylabel('Relative Visual Magnitude')
 plt.savefig(output_image)
