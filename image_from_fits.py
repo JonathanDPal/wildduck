8cc907fc8e7b1a86a0045f741bc7a427573cb014
JonathanDPal <jpal@nd.edu>
2021-12-07 17:34:20 -0500

    working now

--- image_from_fits.py
+++ image_from_fits.py
@@ -1,10 +1,11 @@
 from astropy.io import fits
-from PIL import Image
+from matplotlib import pyplot as plt
 import sys
+import numpy as np
 
-_, fits_file, img_output = sys.argv
+_, fits_file, output_file = sys.argv
 with fits.open(fits_file) as f:
     image_array = f[0].data
-im = Image.fromarray(image_array)
-im = im.convert('RGB')
-im.save(img_output)
+plt.figure(figsize=(10, 10))
+plt.imshow(image_array, vmin = np.percentile(image_array, 40), vmax = np.percentile(image_array, 99.6))
+plt.savefig(output_file)
